592600dda59abf7ac20c5c1cc7ea444e62ec5185
muthurajrk <137182247+muthurajrk@users.noreply.github.com>
2023-07-12 09:43:15 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+/*to login the page by pre defined username and password.
+ex:
+username = "muthuraj"
+password = "muthuraj@123" */
+
+const btn = document.getElementById("button")
+btn.addEventListener('click',display)
+function display(){
+const un = document.getElementById("username").value;
+const pw = document.getElementById("password").value;
+const una = "muthuraj"
+const pwa = "muthuraj@123"
+  if(un===una){
+          if(pw===pwa)
+    alert(`Hi, ${un}`);
+         else
+    alert("Wrong Password");
+  }
+  else
+      alert("Username is Invalid")
+  }
+
+
+
+/*To get the username and password in a empty array*/
+
+const userName = []
+const passWord = []
+const btn = document.getElementById('button')
+btn.addEventListener('click",submit)
+function submit(){
+userName.push(document.getElementById('username'))
+passWord.push(document.getElementById('password'))
+}
